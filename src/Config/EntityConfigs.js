eb12007059cd36b90830f556294611b2eb32e51e
dnsgittest <tim@reservetechinc.com>
2020-07-13 10:26:22 -0700

    removed green player config

--- src/Config/EntityConfigs.js
+++ src/Config/EntityConfigs.js
@@ -1,6 +1,7 @@
 import CONFIG from './Config';
 import PlayerController from '../Controllers/PlayerController';
 import AIController from '../Controllers/AIController';
+import FireballController from '../Controllers/FireballController';
 
 export default [
 	{
@@ -20,33 +21,13 @@ export default [
 		}
 	},
 	{
-		name: 'green-player',
-		url: './sprites/Green-Cap-Character-16x18.png',
-		controller: PlayerController,
-		key_bindings: {
-			up:    {key: 'i'},
-			down:  {key: 'k'},
-			left:  {key: 'j'},
-			right: {key: 'l'},
-			attack_one: {key: 'f'}
-		},
-		position: {
-			x: CONFIG.MAP_DIMENSIONS.WIDTH - (16*2),
-			y: 0
-		}
-	},
-	{
 		name: 'fireball',
 		url: './sprites/fireball-8x8.png',
-		controller: AIController,
+		controller: FireballController,
+		speed: 2.5,
 		dimensions: {
 			width: 8,
 			height: 8
-		},
-		position: {
-			x: 200,
-			y: 50
-		},
-		speed: 1.5
+		}
 	}
 ];
